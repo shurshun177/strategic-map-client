e678712676d3f9fab777c66715be2e473a6d3142
shur <artur.k177@gmail.com>
2019-02-11 01:39:01 +0200

    added snackbar notification

---
+++ src/components/Snackbar.js
@@ -0,0 +1,110 @@
+import React, {Component} from 'react';
+import RestAPI from '../api';
+import { Route, Redirect } from 'react-router';
+import VersionForm from '../components/VersionForm';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import ErrorIcon from '@material-ui/icons/Error';
+import InfoIcon from '@material-ui/icons/Info';
+import CloseIcon from '@material-ui/icons/Close';
+import green from '@material-ui/core/colors/green';
+import amber from '@material-ui/core/colors/amber';
+import IconButton from '@material-ui/core/IconButton';
+import Snackbar from '@material-ui/core/Snackbar';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+import WarningIcon from '@material-ui/icons/Warning';
+import { withStyles } from '@material-ui/core/styles';
+
+const variantIcon = {
+    success: CheckCircleIcon,
+    warning: WarningIcon,
+    error: ErrorIcon,
+    info: InfoIcon,
+};
+
+const styles = theme => ({
+    success: {
+        backgroundColor: green[600],
+    },
+    error: {
+        backgroundColor: theme.palette.error.dark,
+    },
+    info: {
+        backgroundColor: theme.palette.primary.dark,
+    },
+    warning: {
+        backgroundColor: amber[700],
+    },
+    icon: {
+        fontSize: 20,
+    },
+    iconVariant: {
+        opacity: 0.9,
+        marginLeft: theme.spacing.unit,
+    },
+    message: {
+        display: 'flex',
+        alignItems: 'right',
+    },
+    anchor: {
+        vertical: 'top',
+        horizontal: 'left'
+    },
+    root:{
+        position: 'fixed',
+        left: '10px',
+        'z-index': 4000
+    }
+});
+
+
+class Notification extends Component{
+
+
+    render(){
+        const { classes, className, message, onClose, variant, showSnackbar, ...other } = this.props;
+        const Icon = variantIcon[variant];
+    return (
+        <div>
+            <Snackbar
+                anchorOrigin={{vertical: 'top', horizontal: 'left'}}
+                message={
+                    <span id="client-snackbar" className={classes.message}>
+                <Icon className={classNames(classes.icon, classes.iconVariant)}/>
+                        {message}
+                </span>
+                }
+                autoHideDuration={3000}
+                open={showSnackbar}
+                ContentProps={{
+                    classes: {root: classes[variant]}
+                }}
+                onClose={onClose}
+                action={[
+                    <IconButton
+                        key="close"
+                        aria-label="Close"
+                        color="inherit"
+                        className={classes.close}
+                        onClick={onClose}
+                    >
+                        <CloseIcon className={classes.icon}/>
+                    </IconButton>,
+                ]}
+            />
+        </div>
+    );
+    }
+}
+
+Notification.propTypes = {
+    classes: PropTypes.object.isRequired,
+    className: PropTypes.string,
+    message: PropTypes.node,
+    onClose: PropTypes.func,
+    variant: PropTypes.oneOf(['success', 'warning', 'error', 'info']).isRequired,
+    showSnackbar: PropTypes.bool
+};
+
+export default withStyles(styles)(Notification);
--- src/pages/Version-details.js
+++ src/pages/Version-details.js
@@ -1,103 +1,10 @@
-import React, { Component } from 'react';
-import Form from '../components/Form'
+import React, {Component} from 'react';
 import RestAPI from '../api';
 import { Route, Redirect } from 'react-router';
 import VersionForm from '../components/VersionForm';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Button from '@material-ui/core/Button';
-import Slide from '@material-ui/core/Slide';
-import {withStyles} from '@material-ui/core/styles';
-import green from '@material-ui/core/colors/green';
-import CheckCircleIcon from '@material-ui/icons/CheckCircle';
-import ErrorIcon from '@material-ui/icons/Error';
-import InfoIcon from '@material-ui/icons/Info';
-import CloseIcon from '@material-ui/icons/Close';
-import WarningIcon from '@material-ui/icons/Warning';
-import Snackbar from '@material-ui/core/Snackbar';
-import SnackbarContent from '@material-ui/core/SnackbarContent';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import IconButton from '@material-ui/core/IconButton';
+import Notification from '../components/Snackbar';
 
 
-const styles = theme => ({
-  success: {
-    backgroundColor: green[600],
-  },
-  error: {
-    backgroundColor: theme.palette.error.dark,
-  },
-  info: {
-    backgroundColor: theme.palette.primary.dark,
-  },
-  warning: {
-
-  },
-  icon: {
-    fontSize: 20,
-  },
-  iconVariant: {
-    opacity: 0.9,
-    marginRight: theme.spacing.unit,
-  },
-  message: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-});
-
-const variantIcon = {
-  success: CheckCircleIcon,
-  warning: WarningIcon,
-  error: ErrorIcon,
-  info: InfoIcon,
-};
-
-function MySnackbarContent(props) {
-  const { classes, className, message, onClose, variant, ...other } = props;
-  const Icon = variantIcon[variant];
-
-  return (
-    <SnackbarContent
-      className={classNames(classes[variant], className)}
-      aria-describedby="client-snackbar"
-      message={
-        <span id="client-snackbar" className={classes.message}>
-          <Icon className={classNames(classes.icon, classes.iconVariant)} />
-          {message}
-        </span>
-      }
-      action={[
-        <IconButton
-          key="close"
-          aria-label="Close"
-          color="inherit"
-          className={classes.close}
-          onClick={onClose}
-        >
-          <CloseIcon className={classes.icon} />
-        </IconButton>,
-      ]}
-      {...other}
-    />
-  );
-}
-
-MySnackbarContent.propTypes = {
-  classes: PropTypes.object.isRequired,
-  className: PropTypes.string,
-  message: PropTypes.node,
-  onClose: PropTypes.func,
-  variant: PropTypes.oneOf(['success', 'warning', 'error', 'info']).isRequired,
-};
-
-function Transition(props) {
-    return <Slide direction="up" {...props} />;
-}
 class VersionDetails extends Component {
 
     constructor(props) {
@@ -106,7 +13,7 @@ class VersionDetails extends Component {
         this.state = {
             isCreated: false,
             versionNumber: null,
-            open: false
+            showSnackbar: false
         }
     }
 
@@ -137,62 +44,48 @@ class VersionDetails extends Component {
             this.setState((prevState, props) => {
                 return {
                     isCreated: true,
-                    open: true
+                    showSnackbar: true
                 };
             });
         }).catch((error) => {
             this.setState((prevState, props) => {
                 return {
-
-                    open: true
+                    isCreated: false,
+                    showSnackbar: true
                 };
             });
-            //todo if not successful, display an error with toaster
-
-//            alert('such credentials already exist')
         });
     }
 
-    onDialogCancel = () => {
-        this.setState({ open: false});
+    handleClose=(event, reason)=>{
+        this.setState({showSnackbar:false})
     };
 
-    dialogClose = () => {
-        this.setState({open: false});
+    renderNotificationSnackbar=()=>{
+        if (this.state.isCreated){
+            return <Notification  message='kukushonok' variant='success' showSnackbar={this.state.showSnackbar} onClose={this.handleClose} />
+        }
+        else {
+            return <Notification message='an error occured!!!!!' variant='error' showSnackbar={this.state.showSnackbar} onClose={this.handleClose}/>
+        }
     };
-
 
 
     render() {
         return (
+            <>
+            <div>
+            {this.state.showSnackbar? this.renderNotificationSnackbar(): null}
+            </div>
             <div className="main-content">{
 
                 // (<Form handleFormSubmit={this.handleFormSubmit.bind(this)} type='version' versionNumber={this.state.versionNumber}/>)
                     (<VersionForm handleFormSubmit={this.handleFormSubmit.bind(this)} versionNumber={this.state.versionNumber}/>)
             }
 
-
-                <Dialog
-                    open={this.state.open}
-                    TransitionComponent={Transition}
-                    keepMounted
-                    onClose={this.handleClose}
-                    aria-labelledby="alert-dialog-title"
-                    aria-describedby="alert-dialog-description"
-                >
-                    <DialogTitle id="alert-dialog-title">{"צריך לבחור גם מדדים !"}</DialogTitle>
-                    <DialogContent>
-
-                    </DialogContent>
-                    <DialogActions>
-
-                        <Button onClick={this.dialogClose} variant='outlined' color="primary">
-                            אישור
-                        </Button>
-                    </DialogActions>
-                </Dialog>
-            </div>)
+            </div>
+            </>)
     }
 }
 
-export default withStyles(styles) (VersionDetails);
+export default VersionDetails;
